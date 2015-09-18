953144589aac29be5cc7bbd01891985b9d753388
afeng <afeng@kobo.com>
2015-09-18 17:35:20 -0400

    add easy post demo project

---
+++ easypost-csharp-master/EasyPost/Tracker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EasyPost {
+    public class Tracker : IResource {
+        public string id { get; set; }
+        public Nullable<DateTime> created_at { get; set; }
+        public DateTime tracking_updated_at { get; set; }
+        public Nullable<DateTime> updated_at { get; set; }
+        public DateTime est_delivery_date { get; set; }
+        public string mode { get; set; }
+        public string shipment_id { get; set; }
+        public string status { get; set; }
+        public string carrier { get; set; }
+        public string tracking_code { get; set; }
+        public string signed_by { get; set; }
+        public double weight { get; set; }
+        public List<TrackingDetail> tracking_details { get; set; }
+
+        public static Tracker Create(string carrier, string trackingCode) {
+            Request request = new Request("trackers", RestSharp.Method.POST);
+            Dictionary<string, object> parameters = new Dictionary<string, object>() { 
+                { "tracking_code", trackingCode }, { "carrier", carrier }
+            };
+
+            request.AddBody(parameters, "tracker");
+
+            return request.Execute<Tracker>();
+        }
+
+        /// <summary>
+        /// Retrieve a Tracker from its id.
+        /// </summary>
+        /// <param name="id">String representing a Tracker. Starts with "trk_".</param>
+        /// <returns>EasyPost.Tracker instance.</returns>
+        public static Tracker Retrieve(string id) {
+            Request request = new Request("trackers/{id}");
+            request.AddUrlSegment("id", id);
+
+            return request.Execute<Tracker>();
+        }
+    }
+}
---
+++ easypost-csharp-master/EasyPostTest/AddressValidationTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyPost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EasyPostTest
+{
+    [TestClass]
+    public class AddressValidationTest
+    {
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            Client.apiKey = "cueqNZUb3ldeWTNX7MU3Mel8UXtaAMUi";
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Address caAddress = new Address()
+            {
+                company = "Koob",
+                street1 = "1111 TUXEDO court",
+                street2 = "Unit 1",
+                city = "toronto",
+                state = "ON",
+                country = "CA",
+                zip = "M1G3S5"
+            };
+
+            Address usAddress = new Address()
+            {
+                company = "Simpler Postage Inc",
+                street1 = "1645 Townsend Street",
+                street2 = "Unit 1",
+                city = "San Francisco",
+                state = "CA",
+                country = "US",
+                zip = "94107"
+            };
+
+            Address ukAddress = new Address()
+            {
+                company = "Simpler Postage Inc",
+                street1 = "59 GREAT MARLBOROUGH ST BIBIGO K",
+                street2 = "Unit 1",
+                city = "LONDON",
+                country = "GB",
+                zip = "W1F7JY"
+            };
+
+            caAddress.Create();
+
+            caAddress.Verify();
+
+            int a = 1;
+        }
+    }
+}
